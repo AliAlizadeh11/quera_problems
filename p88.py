c0f8aebefc2ad539608a3b5a573f8ebc73208f0b
AliAlizadeh <alializadeh.dev11@gmail.com>
2022-04-16 14:15:27 +0430

    p88 added

---
+++ p88.py
@@ -0,0 +1,15 @@
+n, x ,y = input().split()
+n = int(n)
+x = int(x)
+y = int(y)
+
+i = 0
+while i * x <= n:
+    if (n - (i * x)) % y == 0:
+        a = i
+        b = int((n - (i * x)) / y)
+        print(a, b)
+	i += 1
+
+print(-1)
+
