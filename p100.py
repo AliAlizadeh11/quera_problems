c36a71dfa59d76406768435fcf6cde029b90cd88
AliAlizadeh <alializadeh.dev11@gmail.com>
2022-04-22 23:28:11 +0430

    p99 - p100 added

---
+++ p100.py
@@ -0,0 +1,10 @@
+def find(num1, num2, num3):
+    sum_numbers = num1 + num2 + num3
+    if sum_numbers == 6:
+        return 4
+    elif sum_numbers == 7:
+        return 3
+    elif sum_numbers == 8:
+        return 2
+    else:
+        return 1
---
+++ p99.py
@@ -0,0 +1,5 @@
+n = int(input())
+s1 = 'codecup6'
+m = (n // 8) + 1
+s2 = s1 * m
+print(s2[n-1])
